e026f82163b1e5318f190fd9e9d51fb3db497133
Luckych454 <851022445@qq.com>
2019-09-11 02:21:57 +0800

    Update affineHill.cpp

--- classical_cipher/affineHill.cpp
+++ classical_cipher/affineHill.cpp
@@ -10,22 +10,25 @@
 using namespace std;
 class affineHill{
 private:
-    vector <vector<int> >Lmatrix;
-    vector <int> b;
+    int _m;
+    vector <vector<int> > _Lmatrix;
+    vector <int> _b;
 public:
     affineHill(int m){
-        b = vector<int> (m,0);
-        Lmatrix = vector <vector<int> > (m);
-    }
-    void analysis(string plaintext,string ciphertext){
-        // use 明文和密文更新矩阵
+        _m = m;
+        _b = vector<int> (m,0);
+        _Lmatrix = vector <vector<int> > (m);
+        for(int i = 0;i<m;i++){
+            _Lmatrix[i].resize(m);
+        }
     }
     vector<vector<int> > getL(){
-        return Lmatrix;
+        return _Lmatrix;
     }
     vector<int> getb(){
-        return b;
+        return _b;
     }
+    /*
     int computeDet(vector<vector<int> > ma,int n){
         //按第一行展开计算|A|
         if(n==1) return ma[0][0];
@@ -54,143 +57,143 @@ public:
     }
 
 
-//    vector<vector<int> > computeAdjoint(vector<vector<int> > ma,int n){
-//        if(n==1)
-//        {
-//            vector<vector<int> ans(1);
-//            ans[0][0] = 1;
-//            return ans;
-//        }
-//        int i,j,k,t;
-//        vector<vector<int> temp(n);
-//        vector<vector<int> ans(n);
-//        for(i=0;i<n;i++)
-//        {
-//            for(j=0;j<n;j++)
-//            {
-//                for(k=0;k<n-1;k++)
-//                {
-//                    for(t=0;t<n-1;t++)
-//                    {
-//                        temp[k][t] = ma[k>=i?k+1:k][t>=j?t+1:t];
-//                    }
-//                }
-//                ans[j][i]  =  getA(temp,n-1);
-//                if((i+j)%2 == 1)
-//                {
-//                    ans[j][i] = (26 - ans[j][i]) %26;
-//                }
-//            }
-//        }
-//    }
-//整数矩阵的LU分解失败
-//    vector <vector<int> > getInverse(vector <vector<int> > a,int n){
-//        //使用LU分解求矩阵的逆
-//        vector <vector<int> > L(n),l(n);
-//        vector <vector<int> > U(n),u(n);
-//        vector <vector<int> > out(n);
-//        for(int i = 0;i<n;i++){
-//            U[i].resize(n);
-//            u[i].resize(n);
-//            L[i].resize(n);
-//            l[i].resize(n);
-//            out[i].resize(n);
-//        }
-//        int flag = 1;
-//        int i,j,k;
-//        int s=0,t=0;
-//
-//        // U矩阵的第一行
-//        for(i = 0;i<n;i++) a[0][i] = a[0][i];
-//        // L矩阵的第一列
-//        for(i = 1;i<n;i++) {
-//            a[i][0] = getMultiEqual(a[0][0],a[i][0]);
-//        }
-//        for(k = 1;k<n;k++){
-//            for(j = k;j<n;j++){
-//                s = 0;
-//                for(i = 0;i<k;i++)
-//                    s = s+a[k][i]*a[i][j]; //累加
-//                a[k][j] = (a[k][j] - s)%26;// 计算U矩阵的其他元素
-//            }
-//            for(i = k+1;i<n;i++){
-//                t = 0;
-//                for(j=0;j<k;j++)
-//                    t = t + a[i][j]*a[j][k];//累加
-//                    a[i][k] = getMultiEqual(a[i][k]-t,a[k][k]);
-//                    //a[i][k] = ((a[i][k]-t)/a[k][k])%26; //计算L矩阵的其他元素
-//            }
-//        }
-//        for(int i = 0;i<3;i++){
-//            for(int j = 0;j<3---;j++){
-//                cout << a[i][j]<<" ";
-//            }
-//            cout << endl;
-//        }
-//        for(i = 0;i<n;i++) {
-//            for (j = 0; j < n; j++) {
-//                if (i > j) {
-//                    //如果i>j,计算矩阵的下三角部分，得出L
-//                    L[i][j] = a[i][j];
-//                    U[i][j] = 0;
-//                } else {
-//                    //计算矩阵的shang三角部分，得出U
-//                    U[i][j] = a[i][j];
-//                    if (i == j)
-//                        L[i][j] = 1;
-//                    else
-//                        L[i][j] = 0;
-//                }
-//            }
-//        }
-//        cout << "L" <<endl;
-//        for(int i = 0;i<2;i++){
-//            for(int j = 0;j<2;j++){
-//                cout << L[i][j]<<" ";
-//            }
-//            cout << endl;
-//        }
-//        cout << "U" <<endl;
-//        for(int i = 0;i<3;i++){
-//            for(int j = 0;j<3;j++){
-//                cout << U[i][j]<<" ";
-//            }
-//            cout << endl;
-//        }
-//        for(int i = 0;i<n;i++){
-//            if(U[i][i] == 0) {flag = 0;
-//            cout << "逆矩阵不存在"<<endl;}
-//        }
-//        if(flag ==1){
-//            //求L和U的逆
-//            for(i = 0;i<n;i++){
-//                //求矩阵u的逆
-//                u[i][i] = 1/U[i][i];
-//                for(k = i-1;k>=0;k--){
-//                    s = 0;
-//                    for(j=k+1;j<=i;j++) s = s+U[k][j]*u[j][i];
-//                    //迭代计算，按列倒序依次取每一个值
-//                    u[k][i] = - s/U[k][k];
-//                }
-//            }
-//            for(i = 0;i<n;i++){
-//                //求L矩阵的逆
-//                l[i][i] = 1;
-//                for(k=i+1;k<n;k++){
-//                    for(j = i;j<=k;j++) l[k][i] = l[k][i] - L[k][j]*l[j][i];
-//                }
-//            }
-//        }
-//        //将r u 相乘，得到逆矩阵
-//        for(int i = 0;i<n;i++){
-//            for(int j = 0;j<n;j++){
-//                for(int k = 0;k<n;k++){
-//                    out[i][j] += u[i][k]*l[k][j];
-//                }
-//            }
-//        }
-//        return out;
-//    }
+    vector<vector<int> > computeAdjoint(vector<vector<int> > ma,int n){
+        if(n==1)
+        {
+            vector<vector<int> ans(1);
+            ans[0][0] = 1;
+            return ans;
+        }
+        int i,j,k,t;
+        vector<vector<int> temp(n);
+        vector<vector<int> ans(n);
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                for(k=0;k<n-1;k++)
+                {
+                    for(t=0;t<n-1;t++)
+                    {
+                        temp[k][t] = ma[k>=i?k+1:k][t>=j?t+1:t];
+                    }
+                }
+                ans[j][i]  =  getA(temp,n-1);
+                if((i+j)%2 == 1)
+                {
+                    ans[j][i] = (26 - ans[j][i]) %26;
+                }
+            }
+        }
+    }
+整数矩阵的LU分解失败
+    vector <vector<int> > getInverse(vector <vector<int> > a,int n){
+        //使用LU分解求矩阵的逆
+        vector <vector<int> > L(n),l(n);
+        vector <vector<int> > U(n),u(n);
+        vector <vector<int> > out(n);
+        for(int i = 0;i<n;i++){
+            U[i].resize(n);
+            u[i].resize(n);
+            L[i].resize(n);
+            l[i].resize(n);
+            out[i].resize(n);
+        }
+        int flag = 1;
+        int i,j,k;
+        int s=0,t=0;
+
+        // U矩阵的第一行
+        for(i = 0;i<n;i++) a[0][i] = a[0][i];
+        // L矩阵的第一列
+        for(i = 1;i<n;i++) {
+            a[i][0] = getMultiEqual(a[0][0],a[i][0]);
+        }
+        for(k = 1;k<n;k++){
+            for(j = k;j<n;j++){
+                s = 0;
+                for(i = 0;i<k;i++)
+                    s = s+a[k][i]*a[i][j]; //累加
+                a[k][j] = (a[k][j] - s)%26;// 计算U矩阵的其他元素
+            }
+            for(i = k+1;i<n;i++){
+                t = 0;
+                for(j=0;j<k;j++)
+                    t = t + a[i][j]*a[j][k];//累加
+                    a[i][k] = getMultiEqual(a[i][k]-t,a[k][k]);
+                    //a[i][k] = ((a[i][k]-t)/a[k][k])%26; //计算L矩阵的其他元素
+            }
+        }
+        for(int i = 0;i<3;i++){
+            for(int j = 0;j<3---;j++){
+                cout << a[i][j]<<" ";
+            }
+            cout << endl;
+        }
+        for(i = 0;i<n;i++) {
+            for (j = 0; j < n; j++) {
+                if (i > j) {
+                    //如果i>j,计算矩阵的下三角部分，得出L
+                    L[i][j] = a[i][j];
+                    U[i][j] = 0;
+                } else {
+                    //计算矩阵的shang三角部分，得出U
+                    U[i][j] = a[i][j];
+                    if (i == j)
+                        L[i][j] = 1;
+                    else
+                        L[i][j] = 0;
+                }
+            }
+        }
+        cout << "L" <<endl;
+        for(int i = 0;i<2;i++){
+            for(int j = 0;j<2;j++){
+                cout << L[i][j]<<" ";
+            }
+            cout << endl;
+        }
+        cout << "U" <<endl;
+        for(int i = 0;i<3;i++){
+            for(int j = 0;j<3;j++){
+                cout << U[i][j]<<" ";
+            }
+            cout << endl;
+        }
+        for(int i = 0;i<n;i++){
+            if(U[i][i] == 0) {flag = 0;
+            cout << "逆矩阵不存在"<<endl;}
+        }
+        if(flag ==1){
+            //求L和U的逆
+            for(i = 0;i<n;i++){
+                //求矩阵u的逆
+                u[i][i] = 1/U[i][i];
+                for(k = i-1;k>=0;k--){
+                    s = 0;
+                    for(j=k+1;j<=i;j++) s = s+U[k][j]*u[j][i];
+                    //迭代计算，按列倒序依次取每一个值
+                    u[k][i] = - s/U[k][k];
+                }
+            }
+            for(i = 0;i<n;i++){
+                //求L矩阵的逆
+                l[i][i] = 1;
+                for(k=i+1;k<n;k++){
+                    for(j = i;j<=k;j++) l[k][i] = l[k][i] - L[k][j]*l[j][i];
+                }
+            }
+        }
+        //将r u 相乘，得到逆矩阵
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                for(int k = 0;k<n;k++){
+                    out[i][j] += u[i][k]*l[k][j];
+                }
+            }
+        }
+        return out;
+    }*/
     int addInv(int a){
         return 26-a;
     }
@@ -269,28 +272,75 @@ public:
                 out[i][j] = (e[i][j+n]+26)%26;
             }
         }
+        //求得一个矩阵的逆
         return out;
     }
-
+    bool analysis(string plaintext,string ciphertext){
+        // use 明文和密文更新矩阵
+        vector <vector<int> > x1(_m),x2(_m),y1(_m),y2(_m);
+        for(int i = 0;i<_m;i++){
+            x1[i].resize(_m);
+            y1[i].resize(_m);
+            x2[i].resize(_m);
+            y2[i].resize(_m);
+        }
+        if(plaintext.length()<2*(_m*_m)||ciphertext.length()<2*(_m*_m)) return false;
+        //将字符串转发成数字装入数组中
+        cout <<"+"<<endl;
+        for(int i = 0;i<_m;i++){
+            for(int j = 0;j<_m;j++){
+                x1[i][j] = plaintext[i*_m+j]-'a';
+                x2[i][j] = plaintext[(_m*_m)+i*_m+j]-'a';
+                y1[i][j] = ciphertext[i*_m+j]-'A';
+                y2[i][j] = ciphertext[(_m*_m)+i*_m+j]-'A';
+            }
+        }
+        cout <<"+"<<endl;
+        for(int i = 0;i<_m;i++){
+            for(int j = 0;j<_m;j++){
+                x1[i][j] = (x1[i][j] + addInv(x2[i][j]))%26;
+                y1[i][j] = (y1[i][j] + addInv(y2[i][j]))%26;
+            }
+        }
+        x1 = getInvLinear(x1,_m);
+        cout <<"+"<<endl;
+        for(int i = 0;i<_m;i++){
+            for(int j = 0;j<_m;j++){
+                int t = 0;
+                for(int k = 0;k<_m;k++){
+                    t += x1[i][k] * y1[k][j];
+                }
+                _Lmatrix[i][j] = t%26;
+            }
+        }
+        //用x2,y2得到b
+        x1 = getInvLinear(x1,_m);
+        for(int i = 0;i<_m;i++){
+            int t = 0;
+            for(int j = 0;j<_m;j++){
+                t += x2[0][i] * _Lmatrix[i][j];
+            }
+            _b[i] = (y2[0][i] + addInv(t%26))%26;
+        }
+        for(int i = 0;i<_m;i++){
+            for(int j = 0;j<_m;j++){
+                cout << _Lmatrix[i][j]<<" ";
+            }
+            cout << endl;
+        }
+        for(int j = 0;j<_m;j++){
+            cout << _b[j]<<" ";
+        }
+        cout << endl;
+        return true;
+    }
 };
 int main(){
     string plaintext = "adisplayedequation";
     string ciphertext = "DSRMSIOPLXLJBZULLM";
     int m =  3;
     affineHill a = affineHill(m);
-    vector <vector<int> > mm = {{10,5,12},{3,14,21},{8,9,11}};
+    //vector <vector<int> > mm = {{10,5,12},{3,14,21},{8,9,11}};
 //    vector <vector<int> > mm = {{11,8},{3,7}};
-    for(int i = 0;i<m;i++){
-        for(int j = 0;j<m;j++){
-            cout << mm[i][j]<<" ";
-        }
-        cout << endl;
-    }
-    mm = a.getInvLinear(mm,m);
-    for(int i = 0;i<m;i++){
-        for(int j = 0;j<m;j++){
-            cout << mm[i][j]<<" ";
-        }
-        cout << endl;
-    }
+    a.analysis(plaintext,ciphertext);
 }
